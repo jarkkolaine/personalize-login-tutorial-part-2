ee7f723504998455f70b16a3f180effd6efc2993
Jarkko Laine <jarkko@jarkkolaine.com>
2015-04-24 00:01:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Tuts+ Tutorial: Build a Custom WordPress User Flow — Part 2: New User Registration
+
+#### Instructor: Jarkko Laine
+
+This Github project contains the source files for the Tuts+ tutorial, [Build a Custom WordPress User Flow — Part 2: New User Registration](http://code.tutsplus.com/tutorials/build-a-custom-wordpress-user-flow-part-2-new-user-registration--cms-23810) 
---
+++ personalize-login/templates/login_form.php
@@ -0,0 +1,46 @@
+<div class="login-form-container">
+	<?php if ( $attributes['show_title'] ) : ?>
+		<h2><?php _e( 'Sign In', 'personalize-login' ); ?></h2>
+	<?php endif; ?>
+
+	<!-- Show errors if there are any -->
+	<?php if ( count( $attributes['errors'] ) > 0 ) : ?>
+		<?php foreach ( $attributes['errors'] as $error ) : ?>
+			<p class="login-error">
+				<?php echo $error; ?>
+			</p>
+		<?php endforeach; ?>
+	<?php endif; ?>
+
+	<!-- Show logged out message if user just logged out -->
+	<?php if ( $attributes['logged_out'] ) : ?>
+		<p class="login-info">
+			<?php _e( 'You have signed out. Would you like to sign in again?', 'personalize-login' ); ?>
+		</p>
+	<?php endif; ?>
+
+	<?php if ( $attributes['registered'] ) : ?>
+		<p class="login-info">
+			<?php
+				printf(
+					__( 'You have successfully registered to <strong>%s</strong>. We have emailed your password to the email address you entered.', 'personalize-login' ),
+					get_bloginfo( 'name' )
+				);
+			?>
+		</p>
+	<?php endif; ?>
+
+	<?php
+		wp_login_form(
+			array(
+				'label_username' => __( 'Email', 'personalize-login' ),
+				'label_log_in' => __( 'Sign In', 'personalize-login' ),
+				'redirect' => $attributes['redirect'],
+			)
+		);
+	?>
+
+	<a class="forgot-password" href="<?php echo wp_lostpassword_url(); ?>">
+		<?php _e( 'Forgot your password?', 'personalize-login' ); ?>
+	</a>
+</div>
